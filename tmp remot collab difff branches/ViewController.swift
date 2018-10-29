99ea5c2f48d1799fc2a656a1c2c1661fe1049623
For bootcamp <inocut2@gmail.com>
2018-10-29 13:17:31 -0700

    done

--- tmp remot collab difff branches/ViewController.swift
+++ tmp remot collab difff branches/ViewController.swift
@@ -1,15 +1,8 @@
-//
-//  ViewController.swift
-//  tmp remot collab difff branches
-//
-//  Created by Amir J on 10/29/18.
-//  Copyright © 2018 Cinard - How to Make an App. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    var someFeature = 3
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
